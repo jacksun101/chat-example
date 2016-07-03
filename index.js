d02d52f98871fc0ae64157f01ebb7b01670236c6
Jack Sun <sunhsilu@gmail.com>
2016-07-03 10:38:03 +0800

    Update localhost to 127.0.0.1

--- index.js
+++ index.js
@@ -1,100 +1,102 @@
 var express = require('express'),
-  cluster = require('cluster'),
-  net = require('net'),
-  sio = require('socket.io'),
-  sio_redis = require('socket.io-redis');
+    cluster = require('cluster'),
+    net = require('net'),
+    sio = require('socket.io'),
+    sio_redis = require('socket.io-redis'),
+    os = require('os');
 
 var port = 3000,
-  num_processes = require('os').cpus().length;
+    num_processes = require('os').cpus().length;
 
 if (cluster.isMaster) {
-  // This stores our workers. We need to keep them to be able to reference
-  // them based on source IP address. It's also useful for auto-restart,
-  // for example.
-  var workers = [];
+    // This stores our workers. We need to keep them to be able to reference
+    // them based on source IP address. It's also useful for auto-restart,
+    // for example.
+    var workers = [];
 
-  // Helper function for spawning worker at index 'i'.
-  var spawn = function (i) {
+    // Helper function for spawning worker at index 'i'.
+    var spawn = function (i) {
 
-    //console.log('create a worker ' + i);
+        //console.log('create a worker ' + i);
 
-    workers[i] = cluster.fork();
+        workers[i] = cluster.fork();
 
-    // Optional: Restart worker on exit
-    workers[i].on('exit', function (worker, code, signal) {
-      console.log('respawning worker', i);
-      spawn(i);
-    });
-  };
+        // Optional: Restart worker on exit
+        workers[i].on('exit', function (worker, code, signal) {
+            console.log('respawning worker', i);
+            spawn(i);
+        });
+    };
 
-  // Spawn workers.
-  for (var i = 0; i < num_processes; i++) {
-    spawn(i);
-  }
-
-  // Helper function for getting a worker index based on IP address.
-  // This is a hot path so it should be really fast. The way it works
-  // is by converting the IP address to a number by removing the dots,
-  // then compressing it to the number of slots we have.
-  //
-  // Compared against "real" hashing (from the sticky-session code) and
-  // "real" IP number conversion, this function is on par in terms of
-  // worker index distribution only much faster.
-  var worker_index = function (ip, len) {
-    var s = '';
-    for (var i = 0, _len = ip.length; i < _len; i++) {
-      if (ip[i] !== '.') {
-        s += ip[i];
-      }
+    // Spawn workers.
+    for (var i = 0; i < num_processes; i++) {
+        spawn(i);
     }
 
-    return Number(s) % len;
-  };
+    // Helper function for getting a worker index based on IP address.
+    // This is a hot path so it should be really fast. The way it works
+    // is by converting the IP address to a number by removing the dots,
+    // then compressing it to the number of slots we have.
+    //
+    // Compared against "real" hashing (from the sticky-session code) and
+    // "real" IP number conversion, this function is on par in terms of
+    // worker index distribution only much faster.
+    var worker_index = function (ip, len) {
+        var s = '';
+        for (var i = 0, _len = ip.length; i < _len; i++) {
+            if (ip[i] !== '.') {
+                s += ip[i];
+            }
+        }
 
-  // Create the outside facing server listening on our port.
-  var server = net.createServer({ pauseOnConnect: true }, function (connection) {
-    // We received a connection and need to pass it to the appropriate
-    // worker. Get the worker for this connection's source IP and pass
-    // it the connection.
-    var remoteAddress = connection.remoteAddress.replace(/^.*:/, '');
-    var worker = workers[worker_index(remoteAddress, num_processes)];
-    worker.send('sticky-session:connection', connection);
-  }).listen(port);
+        return Number(s) % len;
+    };
+
+    // Create the outside facing server listening on our port.
+    var server = net.createServer({pauseOnConnect: true}, function (connection) {
+        console.log('A new connection on ' + os.hostname());
+        // We received a connection and need to pass it to the appropriate
+        // worker. Get the worker for this connection's source IP and pass
+        // it the connection.
+        var remoteAddress = connection.remoteAddress.replace(/^.*:/, '');
+        var worker = workers[worker_index(remoteAddress, num_processes)];
+        worker.send('sticky-session:connection', connection);
+    }).listen(port);
 } else {
-  // Note we don't use a port here because the master listens on it for us.
-  var app = new express();
+    // Note we don't use a port here because the master listens on it for us.
+    var app = new express();
 
-  // Here you might use middleware, attach routes, etc.
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-  // Don't expose our internal server to the outside.
-  var server = app.listen(0, 'localhost'),
-    io = sio(server);
-
-  // Tell Socket.IO to use the redis adapter. By default, the redis
-  // server is assumed to be on localhost:6379. You don't have to
-  // specify them explicitly unless you want to change them.
-  io.adapter(sio_redis({ host: 'localhost', port: 6379 }));
-
-  // Here you might use Socket.IO middleware for authorization etc.
-  io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-      io.emit('chat message', msg);
+    // Here you might use middleware, attach routes, etc.
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
     });
-  });
 
-  // Listen to messages sent from the master. Ignore everything else.
-  process.on('message', function (message, connection) {
-    if (message !== 'sticky-session:connection') {
-      return;
-    }
+    // Don't expose our internal server to the outside.
+    var server = app.listen(0, 'localhost'),
+        io = sio(server);
 
-    // Emulate a connection event on the server by emitting the
-    // event with the connection the master sent us.
-    server.emit('connection', connection);
+    // Tell Socket.IO to use the redis adapter. By default, the redis
+    // server is assumed to be on localhost:6379. You don't have to
+    // specify them explicitly unless you want to change them.
+    io.adapter(sio_redis({host: '127.0.0.1', port: 6379}));
 
-    connection.resume();
-  });
+    // Here you might use Socket.IO middleware for authorization etc.
+    io.on('connection', function (socket) {
+        socket.on('chat message', function (msg) {
+            io.emit('chat message', msg);
+        });
+    });
+
+    // Listen to messages sent from the master. Ignore everything else.
+    process.on('message', function (message, connection) {
+        if (message !== 'sticky-session:connection') {
+            return;
+        }
+
+        // Emulate a connection event on the server by emitting the
+        // event with the connection the master sent us.
+        server.emit('connection', connection);
+
+        connection.resume();
+    });
 }
